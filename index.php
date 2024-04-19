28a24256c8b50a5f5ff4dfafc96964d2011a1c59
DAVID DANIEL ARGUELLO CHAVEZ <daviddadnieluni@gmail.com>
2024-04-19 15:29:47 -0600

    login - Carpeta de imagenes,estilos y archivo index

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="estilos/style_login.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <header>
+        
+    </header>
+    <div class="wrapper">
+        <form action="validar_log.php" method="post">
+            <h1>INICIAR SESIÓN</h1>
+            <div class="input-box">
+                <input type="email" id="correo" name="correo" placeholder="Correo electronico" required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" id="pass" name="pass" placeholder="Contraseña" required>
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+           
+            <div class="olvide-c">
+                <a href="#">Registrarse</a>
+            </div>
+
+            <button type="submit" class="btn" >Entrar</button>
+            
+
+
+        </form>
+    </div>
+</body>
+</html>
