0fa93d78e33fb515c679431f042a57f1566416f4
SlayerKing <alonsosalinas511@gmail.com>
2024-04-20 09:30:02 -0600

    Se dio estructura basica al index del adimintrador para trabajar con sweetalert2

---
+++ ADMINISTRADOR/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+</head>
+
+<body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</body>
+
+</html>
