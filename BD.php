8f3b9be2e78e4b3d4260e5e2090b06b5555fa7af
Jose de Jesus Hernandez Luis <jesusytm29@gmail.com>
2024-04-23 13:55:08 -0600

    conexion

---
+++ BD.php
@@ -0,0 +1,8 @@
+<?php
+    $con = mysqli_connect('localhost', 'root', '', 'vinculacion_ingles');
+        if (!$con){
+            echo('Conexion fallida');
+        }else{
+            
+        }
+?>
