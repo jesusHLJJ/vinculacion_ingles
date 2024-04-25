627a701f48a983a95d80052a1d04d558f2c6715e
SlayerKing <alonsosalinas511@gmail.com>
2024-04-24 18:05:12 -0600

    Todos mis cambios

--- ADMINISTRADOR/CONTROLADORES/datos_niveles.php
+++ ADMINISTRADOR/CONTROLADORES/datos_niveles.php
@@ -1,3 +1,18 @@
 <?php
+include "../../BD.php";
 
+$sql = "SELECT grupos.id_grupo, grupos.nivel, grupos.nombre_grupo, profesores.nombres, grupos.cupo_maximo, grupos.modalidad, profesores.turno, grupos.horario FROM grupos JOIN profesores ON grupos.id_profesor = profesores.id_profesor";
+$result = $conexion->query($sql);
+
+$niveles = [];
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $niveles[] = $row;
+    }
+}
+
+$conexion->close();
+
+echo json_encode($niveles)
 ?>
