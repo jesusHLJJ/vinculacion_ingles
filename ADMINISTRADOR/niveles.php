3a727fea8ceb5f0e982a4929816fd4ac22a36999
SlayerKing <alonsosalinas511@gmail.com>
2024-04-30 13:08:58 -0600

    si

--- ADMINISTRADOR/niveles.php
+++ ADMINISTRADOR/niveles.php
@@ -54,6 +54,10 @@
             </table>
         </div>
     </div>
+
+    <div class="close">
+        <a href="../CONTROLADORES/cerrar_sesion.php"><i class="fa-solid fa-right-from-bracket"></i></a>
+    </div>
     
     <!--JQuery-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
