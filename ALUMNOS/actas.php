b72e32834fa7a237fde6aeb5de5ebd06d0d377f2
DAVID DANIEL ARGUELLO CHAVEZ <daviddadnieluni@gmail.com>
2024-06-10 21:07:12 -0600

    ALUMNOS FINAL

--- ALUMNOS/actas.php
+++ ALUMNOS/actas.php
@@ -9,6 +9,12 @@ if (!isset($_SESSION['tipo'])) {
         header('location: ../');
     }
 }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['volver'])) {
+        header("Location:alumnos.php");
+    }
+}
 error_reporting(0);
 ini_set('display_errors', 0);
 
@@ -26,8 +32,8 @@ if ($result->num_rows > 0) {
     $expediente = $fila['id_expediente'];
 }
 
-$ruta_archivo = "../CONSTANCIAS/$grupo/$expediente"."acta.pdf";
-$file_exists = file_exists($ruta_archivo);
+$directory = "../CONSTANCIAS/$grupo";
+$pdfFiles = glob($directory . "/*.pdf");
 ?>
 
 <!DOCTYPE html>
@@ -37,30 +43,44 @@ $file_exists = file_exists($ruta_archivo);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VER ACTAS</title>
+    <link rel="stylesheet" href="styles.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <link rel="stylesheet" href="estilos/actas.css">
 </head>
 
 <body>
-    <?php
-    if ($file_exists) {
-        header('Content-type: application/pdf');
-        readfile($ruta_archivo);
-    } else {
-        echo "<script>var fileExists = false;</script>";
-    }
-    ?>
+    <div class="container">
+        <h1>VER ACTAS</h1>
+        <?php
+        if ($pdfFiles) {
+            echo '<table>';
+            echo '<tr><th>Archivo PDF</th></tr>';
+            foreach ($pdfFiles as $file) {
+                echo "<tr><td><a href='$file' target='_blank'>" . basename($file) . "</a></td></tr>";
+            }
+            echo '</table>';
+        } else {
+            echo "<script>var fileExists = false;</script>";
+        }
+        ?>
+    
     <script>
         if (typeof fileExists !== 'undefined' && !fileExists) {
             Swal.fire({
                 title: 'NO HAY INFORMACIÓN DISPONIBLE',
                 icon: 'error',
                 confirmButtonText: 'Aceptar',
-                confirmButtonColor:'#ffbb00'
+                confirmButtonColor: '#ffbb00'
             }).then(() => {
                 window.location.href = '../ALUMNOS/alumnos.php';
             });
         }
     </script>
+    <br><br>
+     <form action="" method="post">
+        <input type="submit" id="volver" name="volver" value=VOLVER>
+    </form>
+    </div>
 </body>
 
 </html>
